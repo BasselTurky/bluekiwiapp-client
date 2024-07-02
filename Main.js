f39a071b07e8eb56199ac818f7f33fc70aaee257
BasselTurky <85626321+BasselTurky@users.noreply.github.com>
2024-07-02 21:41:28 +0300

    giveaway error fix

--- Main.js
+++ Main.js
@@ -1,6 +1,7 @@
 import { StyleSheet, ImageBackground } from "react-native";
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import * as NavigationBar from "expo-navigation-bar";
 
 import { SocketProvider } from "./Pages/SocketContext/SocketContext";
 import { ToastProvider } from "react-native-toast-notifications";
@@ -35,13 +36,17 @@ import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import { z } from "./utils/scaling";
 
+NavigationBar.setBackgroundColorAsync("transparent");
+
 const Stack = createNativeStackNavigator();
 
-async function save(key, value) {
+// Function to save a value to secure storage
+async function saveToSecureStore(key, value) {
   await SecureStore.setItemAsync(key, value);
 }
 
-async function deleteValueFor(key) {
+// Function to delete a value from secure storage
+async function deleteFromSecureStore(key) {
   await SecureStore.deleteItemAsync(key);
 }
 
@@ -71,161 +76,116 @@ export default function Main() {
     }
   }, [fontsLoaded]);
 
+  // Main function to check for an authentication token
   async function checkForToken() {
     try {
-      // first check if user is signedIn with google:
-      const isSignedIn = await GoogleSignin.isSignedIn();
-
-      if (isSignedIn) {
-        try {
-          const tokens = await GoogleSignin.getTokens();
-
-          let response = await fetch(
-            `${global.server_address}/auth/sign-google-idToken`,
-            {
-              method: "POST",
-              headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({
-                idToken: tokens.idToken,
-              }),
-            }
-          );
-
-          if (!response.ok) {
-            try {
-              const errorData = await response.json();
-              console.error("Error:", errorData.error);
-
-              deleteValueFor("token");
-              await GoogleSignin.signOut();
-              dispatch(setAuth(false));
-              await SplashScreen.hideAsync();
-            } catch (error) {
-              // If parsing fails, log a generic error message
-              console.error(
-                "Failed to parse error response:",
-                response.statusText
-              );
-            }
-
-            deleteValueFor("token");
-            await GoogleSignin.signOut();
-            dispatch(setAuth(false));
-            await SplashScreen.hideAsync();
-          } else {
-            let data = await response.json();
-            // token may be expired
-
-            await save("token", data.token);
-
-            dispatch(setAuth("google"));
-          }
-        } catch (error) {
-          console.error(error);
-          dispatch(setAuth(false));
-        }
-
-        return;
+      // Check if the user is signed in with Google
+      if (await GoogleSignin.isSignedIn()) {
+        await handleGoogleSignIn();
       } else {
-        let result = await SecureStore.getItemAsync("token");
-
-        if (!result) {
-          // if False : set 'auth' to false, which means no session available, user will be taken to Login screen.
-
-          console.log("Token fetch results: ", result, "no token available");
-
-          dispatch(setAuth(false));
-          // setLoading(false);
-          await SplashScreen.hideAsync();
-          return;
-        } else {
-          // if True : send the token to the backend for verification
-
-          console.log("Token result: ", result);
-
-          let response = await fetch(
-            `${global.server_address}/auth/check-token`,
-            {
-              method: "POST",
-              headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({ token: result }),
-            }
-          );
-
-          let jsonData = await response.json();
-          // remove all but error condition
-          if (jsonData === "error") {
-            // Clear session, restart start from login page
-
-            dispatch(setAuth(false));
-            await deleteValueFor("token");
-            // setLoading(false);
-            await SplashScreen.hideAsync();
-          } else if (jsonData === "pass") {
-            // Request refresh token
-
-            let refresh_token_response = await fetch(
-              `${global.server_address}/auth/refresh-token`,
-              {
-                method: "POST",
-                headers: {
-                  Accept: "application/json",
-                  "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ token: result }),
-              }
-            );
-
-            let data = await refresh_token_response.json();
-
-            // remove error condition
-            if (data.type === "expired" || data.type === "error") {
-              console.log("Error ID: E004: ", data.message);
-              // Clear session, restart start from login page
-
-              dispatch(setAuth(false));
-              await deleteValueFor("token");
-              await SplashScreen.hideAsync();
-            } else if (data.type === "pass") {
-              // Save the new generated token, let user it
-
-              dispatch(setAuth("default"));
-              await save("token", data.token);
-            } else {
-              // Clear session, restart start from login page
-              console.log("ErrorID E003");
-              alert("Error ID: E003");
-              // To do: Edit
-              dispatch(setAuth(false));
-              await deleteValueFor("token");
-              await SplashScreen.hideAsync();
-            }
-          } else {
-            // Clear session, restart start from login page
-            console.log("ErrorID E002");
-            alert("Error ID: E002");
-
-            dispatch(setAuth(false));
-            await deleteValueFor("token");
-            await SplashScreen.hideAsync();
-          }
-        }
+        // If not signed in with Google, check secure storage for a token
+        await handleSecureStoreToken();
       }
     } catch (error) {
-      // Clear session, restart start from login page
-      console.log("ErrorID E001: ", error);
-      alert("Error ID: E001");
-
-      dispatch(setAuth(false));
-      await deleteValueFor("token");
-      await SplashScreen.hideAsync();
+      // Handle errors and reset authentication state
+      resetAuth();
     }
+  }
+
+  // Function to handle Google Sign-In
+  async function handleGoogleSignIn() {
+    try {
+      const tokens = await GoogleSignin.getTokens(); // Get tokens from Google Sign-In
+      const response = await sendGoogleTokenToServer(tokens.idToken); // Send token to server for verification
+
+      if (response.ok) {
+        const data = await response.json(); // Parse response data
+        await saveToSecureStore("token", data.token); // Save token to secure storage
+        dispatch(setAuth("google")); // Set authentication state to Google
+      } else {
+        resetAuth(); // Reset authentication if response is not OK
+      }
+    } catch (error) {
+      resetAuth(); // Reset authentication on error
+    }
+  }
+
+  // Function to send Google token to server
+  async function sendGoogleTokenToServer(idToken) {
+    return fetch(`${global.server_address}/auth/sign-google-idToken`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ idToken }),
+    });
+  }
+
+  // Function to handle token from secure storage
+  async function handleSecureStoreToken() {
+    const token = await SecureStore.getItemAsync("token"); // Get token from secure storage
+
+    if (!token) {
+      resetAuth(); // Reset authentication if no token found
+      return;
+    }
+
+    const response = await sendTokenToServer(token); // Send token to server for verification
+    const jsonData = await response.json(); // Parse response data
+
+    if (jsonData === "error") {
+      resetAuth(); // Reset authentication on error response
+    } else if (jsonData === "pass") {
+      await refreshAuthToken(token); // Refresh authentication token if pass response
+    } else {
+      resetAuth(); // Reset authentication for any other response
+    }
+  }
+
+  // Function to send token to server for verification
+  async function sendTokenToServer(token) {
+    return fetch(`${global.server_address}/auth/check-token`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ token }),
+    });
+  }
+
+  // Function to refresh authentication token
+  async function refreshAuthToken(token) {
+    const response = await fetch(
+      `${global.server_address}/auth/refresh-token`,
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ token }),
+      }
+    );
+
+    const data = await response.json(); // Parse response data
+
+    if (data.type === "expired" || data.type === "error") {
+      resetAuth(); // Reset authentication if token expired or error
+    } else if (data.type === "pass") {
+      await saveToSecureStore("token", data.token); // Save new token to secure storage
+      dispatch(setAuth("default")); // Set authentication state to default
+    } else {
+      resetAuth(); // Reset authentication for any other response
+    }
+  }
+
+  // Function to reset authentication state
+  async function resetAuth() {
+    dispatch(setAuth(false)); // Set authentication state to false
+    await deleteFromSecureStore("token"); // Delete token from secure storage
+    await SplashScreen.hideAsync(); // Hide splash screen
   }
 
   if (auth) {
@@ -315,3 +275,167 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 });
+
+// async function checkForToken() {
+//   try {
+//     // first check if user is signedIn with google:
+//     const isSignedIn = await GoogleSignin.isSignedIn();
+
+//     if (isSignedIn) {
+//       console.log("Main: check isSingedIn value: ", isSignedIn);
+//       try {
+//         const tokens = await GoogleSignin.getTokens();
+//         console.log(`Main: GoogleSignIn token: ${tokens}`);
+//         let response = await fetch(
+//           `${global.server_address}/auth/sign-google-idToken`,
+//           {
+//             method: "POST",
+//             headers: {
+//               Accept: "application/json",
+//               "Content-Type": "application/json",
+//             },
+//             body: JSON.stringify({
+//               idToken: tokens.idToken,
+//             }),
+//           }
+//         );
+//         console.log(`Main: Token sent to "/auth/sign-google-idToken"`);
+//         if (!response.ok) {
+//           try {
+//             console.log(`Main: sign-google-idToken response is not Ok`);
+//             const errorData = await response.json();
+//             console.error("Main: Error:", errorData.error);
+
+//             deleteValueFor("token");
+//             await GoogleSignin.signOut();
+//             dispatch(setAuth(false));
+//             await SplashScreen.hideAsync();
+//             console.log(`Main: Token deleted, Google SignOut, Auth set to False, Splash down`);
+//           } catch (error) {
+//             // If parsing fails, log a generic error message
+//             console.error(
+//               "Main: Failed to parse error response:",
+//               response.statusText
+//             );
+//           }
+
+//           deleteValueFor("token");
+//           await GoogleSignin.signOut();
+//           dispatch(setAuth(false));
+//           await SplashScreen.hideAsync();
+
+//         } else {
+//           let data = await response.json();
+//           // token may be expired
+
+//           await save("token", data.token);
+
+//           dispatch(setAuth("google"));
+//         }
+//       } catch (error) {
+//         console.error(error);
+//         dispatch(setAuth(false));
+//       }
+
+//       return;
+
+//     } else {
+//       let result = await SecureStore.getItemAsync("token");
+
+//       if (!result) {
+//         // if False : set 'auth' to false, which means no session available, user will be taken to Login screen.
+
+//         console.log("Token fetch results: ", result, "no token available");
+
+//         dispatch(setAuth(false));
+//         // setLoading(false);
+//         await SplashScreen.hideAsync();
+//         return;
+//       } else {
+//         // if True : send the token to the backend for verification
+
+//         console.log("Token result: ", result);
+
+//         let response = await fetch(
+//           `${global.server_address}/auth/check-token`,
+//           {
+//             method: "POST",
+//             headers: {
+//               Accept: "application/json",
+//               "Content-Type": "application/json",
+//             },
+//             body: JSON.stringify({ token: result }),
+//           }
+//         );
+
+//         let jsonData = await response.json();
+//         // remove all but error condition
+//         if (jsonData === "error") {
+//           // Clear session, restart start from login page
+
+//           dispatch(setAuth(false));
+//           await deleteValueFor("token");
+//           // setLoading(false);
+//           await SplashScreen.hideAsync();
+//         } else if (jsonData === "pass") {
+//           // Request refresh token
+
+//           let refresh_token_response = await fetch(
+//             `${global.server_address}/auth/refresh-token`,
+//             {
+//               method: "POST",
+//               headers: {
+//                 Accept: "application/json",
+//                 "Content-Type": "application/json",
+//               },
+//               body: JSON.stringify({ token: result }),
+//             }
+//           );
+
+//           let data = await refresh_token_response.json();
+
+//           // remove error condition
+//           if (data.type === "expired" || data.type === "error") {
+//             console.log("Error ID: E004: ", data.message);
+//             // Clear session, restart start from login page
+
+//             dispatch(setAuth(false));
+//             await deleteValueFor("token");
+//             await SplashScreen.hideAsync();
+//           } else if (data.type === "pass") {
+//             // Save the new generated token, let user it
+
+//             dispatch(setAuth("default"));
+//             await save("token", data.token);
+//           } else {
+//             // Clear session, restart start from login page
+//             console.log("ErrorID E003");
+//             alert("Error ID: E003");
+//             // To do: Edit
+//             dispatch(setAuth(false));
+//             await deleteValueFor("token");
+//             await SplashScreen.hideAsync();
+//           }
+//         } else {
+//           // Clear session, restart start from login page
+//           console.log("ErrorID E002");
+//           alert("Error ID: E002");
+
+//           dispatch(setAuth(false));
+//           await deleteValueFor("token");
+//           await SplashScreen.hideAsync();
+//         }
+//       }
+//     }
+//   } catch (error) {
+//     // Clear session, restart start from login page
+//     console.log("ErrorID E001: ", error);
+//     alert("Error ID: E001");
+
+//     dispatch(setAuth(false));
+//     await deleteValueFor("token");
+//     await SplashScreen.hideAsync();
+//   }
+// }
+
+// Function to check for authentication token
